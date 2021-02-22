34f07eca4fcf58a3299864e80b9f1612148d76c3
jxsl13 <jxsl13@gmail.com>
2021-02-22 22:37:46 +0100

    init of in memory store

---
+++ init_key_value_store.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/jszwec/csvutil"
+)
+
+func initializeKeyValueStore(cs *CSet, dataFolderPath string) error {
+	err := filepath.Walk(dataFolderPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			return nil
+		}
+		if !isValidSuffix(path) {
+			return nil
+		}
+		b, err := ioutil.ReadFile(path)
+		if err != nil {
+			return nil
+		}
+		var list []Reason
+		err = csvutil.Unmarshal(b, list)
+		if err != nil {
+			return nil
+		}
+		log.Printf("Read %d lines in: %s\n", len(list), path)
+		for _, reason := range list {
+			cs.AddFromCSV(reason)
+		}
+		return nil
+	})
+	return err
+}
+
+var suffix = []string{".csv"}
+
+func isValidSuffix(path string) bool {
+	for _, s := range suffix {
+		if strings.HasSuffix(path, s) {
+			return true
+		}
+	}
+	return false
+}
--- initialize_key_value_store.go
+++
@@ -1,5 +0,0 @@
-package main
-
-func initializeKeyValueStore(dataFolderPath string) error {
-	return nil
-}
